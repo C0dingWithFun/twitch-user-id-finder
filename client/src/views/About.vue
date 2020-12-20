98e27f30d8e98515cc97f8707e7a17c53f6b61a3
Coding With Fun <admin@codingwith.fun>
2020-12-19 20:01:32 -0600

    completed about page on client

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>
+      This website is created to help me with makine one of my tutorial video on my YouTube channel:
+      <a href="https://youtube.codingwith.fun">Coding With Fun</a>. To stay focused on particular
+      topic and avoid explaining unrelated code (how to fetch userId), I created this simple open
+      source website to keep it simple! If you find any bug on this site or if you want to redesign
+      it, feel free to create issue or PR on the GitHub repo
+      <a href="https://github.com/C0dingWithFun/twitch-user-id-finder">here</a>.
+
+      <br />
+      Thank you so much for visiting this site ❤️ !
+    </p>
   </div>
 </template>
+
+<style scoped>
+.about {
+  width: 60%;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 40px;
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
