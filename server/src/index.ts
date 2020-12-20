058f85a7efc89319e4a47ce4fd7a2ad914bb4890
Coding With Fun <admin@codingwith.fun>
2020-12-19 21:45:19 -0600

    fixed dotconfig issues

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,22 @@
 /* eslint-disable */
 import dotenv from 'dotenv';
-import path from 'path';
+// import path from 'path';
 
-const dotenvConfig = dotenv.config({
-  path: path.resolve(process.cwd(), '.env'),
-});
-if (dotenvConfig.error) {
-  throw dotenvConfig.error;
-}
+// const dotenvConfig = dotenv.config({
+//   path: path.resolve(process.cwd(), '.env'),
+// });
+// if (dotenvConfig.error) {
+//   throw dotenvConfig.error;
+// }
+
+dotenv.config();
 
 import { __PORT__ } from './constants';
 import app from './app';
 import './redis.db';
 
 app.listen(__PORT__, () => {
-  console.info(`🎉 Application has started sucessfully at http://localhost:${__PORT__}`);
+  console.info(
+    `🎉 Application has started sucessfully at http://localhost:${__PORT__}`
+  );
 });
