bf03b684c528454b47d88335e9467eda0a5624a4
Coding With Fun <admin@codingwith.fun>
2020-12-19 21:56:35 -0600

    fixed redis client options

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,5 +1,8 @@
 export const __PROD__ = process.env.NODE_ENV === 'production';
 export const __PORT__ = process.env.PORT ? Number(process.env.PORT) : 9000;
+export const __REDIS_URL__ = process.env.REDIS_URL
+  ? process.env.REDIS_URL
+  : 'redis://localhost:6379';
 
 export const __TWITCH_CLIENT_ID__ = process.env.TWITCH_CLIENT_ID?.toString();
 export const __TWITCH_CLIENT_SECRET__ = process.env.TWITCH_CLIENT_SECRET?.toString();
--- server/src/redis.db.ts
+++ server/src/redis.db.ts
@@ -1,6 +1,7 @@
 import redis, { RedisClient } from 'redis';
+import { __REDIS_URL__ } from './constants';
 
-const redisClient: RedisClient = redis.createClient();
+const redisClient: RedisClient = redis.createClient(__REDIS_URL__);
 
 redisClient.on('error', (error) => {
   console.error('🔴 Failed to connect to the redis server :(');
